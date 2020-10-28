62da29b69b2206a64617e5bfa8d03c14af81a882
Jesus Manteca <jesusmanteca@gmail.com>
2020-10-28 15:47:12 -0500

    modified routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Post, User, Comment, Vote } = require('../../models');
 
-// get all users
+// get all users posts
 router.get('/', (req, res) => {
   console.log('======================');
   Post.findAll({
